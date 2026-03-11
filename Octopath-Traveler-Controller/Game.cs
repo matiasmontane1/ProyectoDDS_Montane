48c373f8d9e473c1b6a497992aa606bea54907bd
matiasmontane1 <matiasmontane1@uc.cl>
2026-03-11 12:13:55 -0300

    chore: commit inicial con el código base del enunciado

---
+++ Octopath-Traveler-Controller/Game.cs
@@ -0,0 +1,17 @@
+using Octopath_Traveler_View;
+
+namespace Octopath_Traveler;
+
+public class Game
+{
+    private View _view;
+    public Game(View view, string teamsFolder)
+    {
+        _view = view;
+    }
+
+    public void Play()
+    {
+        throw new NotImplementedException();
+    }
+}
